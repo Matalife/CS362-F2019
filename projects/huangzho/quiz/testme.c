a70b78d981aad8ed9aca310e57576e42dbb864fd
Matalife <40774236+Matalife@users.noreply.github.com>
2019-11-10 19:55:13 -0800

    Add files via upload

--- projects/huangzho/quiz/testme.c
+++ projects/huangzho/quiz/testme.c
@@ -1,3 +1,10 @@
+/*********************************************
+* Author: Zhongkai Huang
+* File: testme.c
+* Time: 2019/11/10
+* Description: Randomly test
+*********************************************/
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -5,14 +12,21 @@
 
 char inputChar()
 {
-    // TODO: rewrite this function
-    return ' ';
+    char ran = (rand() % 94) + 32;
+    return ran;
 }
 
 char *inputString()
 {
-    // TODO: rewrite this function
-    return "";
+	static char in[6];
+	int x = 0;
+	while (x < 5)
+	{
+		in[x] = (rand() % 16) + 101;
+		x++;
+	}
+
+    return in;
 }
 
 void testme()
@@ -28,19 +42,16 @@ void testme()
     s = inputString();
     printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
 
-    if (c == '[' && state == 0) state = 1;
-    if (c == '(' && state == 1) state = 2;
-    if (c == '{' && state == 2) state = 3;
-    if (c == ' '&& state == 3) state = 4;
-    if (c == 'a' && state == 4) state = 5;
-    if (c == 'x' && state == 5) state = 6;
-    if (c == '}' && state == 6) state = 7;
-    if (c == ')' && state == 7) state = 8;
-    if (c == ']' && state == 8) state = 9;
-    if (s[0] == 'r' && s[1] == 'e'
-       && s[2] == 's' && s[3] == 'e'
-       && s[4] == 't' && s[5] == '\0'
-       && state == 9)
+    if (c == '[' && state == 0)	state = 1;
+    if (c == '(' && state == 1)	state = 2;
+    if (c == '{' && state == 2)	state = 3;
+    if (c == ' '&& state == 3) 	state = 4;
+    if (c == 'a' && state == 4)	state = 5;
+    if (c == 'x' && state == 5)	state = 6;
+    if (c == '}' && state == 6)	state = 7;
+    if (c == ')' && state == 7)	state = 8;
+    if (c == ']' && state == 8)	state = 9;
+    if (s[0] == 'r' && s[1] == 'e' && s[2] == 's' && s[3] == 'e' && s[4] == 't' && s[5] == '\0' && state == 9)
     {
       printf("error ");
       exit(200);
